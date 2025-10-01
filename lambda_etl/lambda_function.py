07c14c3f512ac31baec92d5f8ebd983eb214f017
DacheOA <dachely.otero.argote@gmail.com>
2025-09-30 19:01:25 -0600

    Se agrega tabla dynamodb y se da permisos a lambda para escribir en esta

--- lambda_etl/lambda_function.py
+++ lambda_etl/lambda_function.py
@@ -1,5 +1,21 @@
+import boto3
+import os
+
 def lambda_handler(event, context):
+    table_name = os.environ.get("TABLE_NAME")
+    dynamodb = boto3.resource('dynamodb')
+    #table = dynamodb.Table('ResultsTable')
+    table = dynamodb.Table(table_name)
+
+    # Insertar un ejemplo de datos
+    table.put_item(
+        Item={
+            'user_id': '123',
+            'transactions_count': 10,
+            'total_amount': 250
+        }
+    )
     return{
         'statusCode': 200,
-        'body': 'Hello, World!'
+        'body': 'Datos insertados correctamente en DynamoDB'
     }
--- test_v1/test_v1_stack.py
+++ test_v1/test_v1_stack.py
@@ -3,7 +3,8 @@ from aws_cdk import (
     Stack,
     # aws_sqs as sqs,
     aws_s3 as s3,
-    aws_lambda as _lambda
+    aws_lambda as _lambda,
+    aws_dynamodb as dynamodb,
 )
 from constructs import Construct
 
@@ -13,14 +14,29 @@ class TestV1Stack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # Crear el S3 Bucket para cargar los CSVs
-        self.data_bucket = s3.Bucket(self, "DataBucket")
+        data_bucket = s3.Bucket(self, "DataBucket")
+
+        # Crear tabla en dynamoDB para guardar el contenido de los CSVs
+        results_table = dynamodb.Table(
+            self, "ResultsTable",
+            partition_key=dynamodb.Attribute(
+                name="user_id",
+                type=dynamodb.AttributeType.STRING
+            ),
+            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
+            #billing=dynamodb.Billing.on_demand(),
+        )
 
         # Crear la función lambda para leer y procesar los CSVs que se encuentren en el S3
-        self.etl_lambda = _lambda.Function(
+        etl_lambda = _lambda.Function(
             self, "ETLFunction",
-            runtime= _lambda.Runtime.PYTHON_3_12,
+            runtime= _lambda.Runtime.PYTHON_3_9,
             handler="lambda_function.lambda_handler",
-            code=_lambda.Code.from_asset("lambda_etl")
+            code=_lambda.Code.from_asset("lambda_etl"),
+            environment={"TABLE_NAME": results_table.table_name},
         )
+
+        # Dar permisos a lambda para escribir en dynamodb
+        results_table.grant_write_data(etl_lambda)
 
 
